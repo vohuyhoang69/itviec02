7a6d52d007b732695bfa684b587c3524065ff495
vohuyhoang-dev <bakaboom01@gmail.com>
2025-06-27 19:59:55 +0700

    Add files via upload

---
+++ pages/Doanh_nghiep.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6a3e4603a460424f4525b7b0a09da46110a456d91c575678a541e18417ca1bd
+size 5996
---
+++ pages/Ung_vien_xin_viec.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:62c4730163b80ca6f8e71509e6be8be35ad5b9a9c96c22e00b724e8a6c12b686
+size 9284
